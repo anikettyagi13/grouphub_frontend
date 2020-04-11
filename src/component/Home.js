be198fd7b15c32c3fe5ce280897dc19a0d312707
anikettyagi13 <anikettyagi13@gmail.com>
2020-04-11 12:41:16 +0530

    added fetching of data

--- src/component/Home.js
+++ src/component/Home.js
@@ -36,20 +36,32 @@ class Home extends React.Component{
                 throw new Error();
             }
         }).then((data)=>{
-            this.setState({
-                isLoading:false,
-                posts:data.posts,
-                username:data.username,
-                groupnames:data.groupnames
-            },()=>{
-                if(this.state.posts.length<6){
-                    this.state.limit+=1;
-                    if(this.state.limit==10){
-                        this.state.notMore=true;
+            var posts = this.state.posts;
+            posts.push(...data.posts);
+            if(data.posts.length>0){
+                this.setState({
+                    isLoading:false,
+                    posts:posts,
+                    username:data.username,
+                    groupnames:data.groupnames
+                },()=>{
+                    if(this.state.posts.length<6){
+                        this.state.limit+=1;
+                        if(this.state.limit===5){
+                            this.state.notMore=true;
+                        }
+                        this.componentDidMount();
                     }
-                    this.componentDidMount();
+                })
+            }else{
+                if(this.state.limit===5){
+                    this.setState({
+                        notMore:true
+                    })
                 }
-            })
+                this.state.limit+=1;
+                this.componentDidMount();
+            }
             
         }).catch(()=>{
             this.setState({
